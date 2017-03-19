f160ecb0f7211f128ede2abba20779a53ec5278f
OlgaKuklina <okuklina@hotmail.com>
2017-03-18 17:20:16 -0700

    - implemented database and content provider

---
+++ app/src/main/java/com/oklab/githubjourney/data/ActivityItemsProvider.java
@@ -0,0 +1,147 @@
+package com.oklab.githubjourney.data;
+
+import android.content.ContentProvider;
+import android.content.ContentProviderOperation;
+import android.content.ContentProviderResult;
+import android.content.ContentValues;
+import android.content.OperationApplicationException;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by olgakuklina on 2017-03-18.
+ */
+
+public class ActivityItemsProvider extends ContentProvider {
+    private SQLiteOpenHelper mOpenHelper;
+    interface Tables {
+        String ITEMS = "items";
+    }
+    private static final int ITEMS = 0;
+    private static final int ITEMS_ID = 1;
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+
+    private static UriMatcher buildUriMatcher() {
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority = ActivityItemsContract.CONTENT_AUTHORITY;
+        matcher.addURI(authority, "items", ITEMS);
+        matcher.addURI(authority, "items/#", ITEMS_ID);
+        return matcher;
+    }
+
+    @Override
+    public boolean onCreate() {
+        mOpenHelper = new ActivityItemsDatabase(getContext());
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        final SQLiteDatabase db = mOpenHelper.getReadableDatabase();
+        final SelectionBuilder builder = buildSelection(uri);
+        Cursor cursor = builder.where(selection, selectionArgs).query(db, projection, sortOrder);
+        if (cursor != null) {
+            cursor.setNotificationUri(getContext().getContentResolver(), uri);
+        }
+        return cursor;
+    }
+
+    @Nullable
+    @Override
+    public String getType(Uri uri) {
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case ITEMS:
+                return ActivityItemsContract.Items.CONTENT_TYPE;
+            case ITEMS_ID:
+                return ActivityItemsContract.Items.CONTENT_ITEM_TYPE;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case ITEMS: {
+                final long _id = db.insertOrThrow(Tables.ITEMS, null, values);
+                getContext().getContentResolver().notifyChange(uri, null);
+                return ActivityItemsContract.Items.buildItemUri(_id);
+            }
+            default: {
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+            }
+        }
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        final SelectionBuilder builder = buildSelection(uri);
+        getContext().getContentResolver().notifyChange(uri, null);
+        return builder.where(selection, selectionArgs).delete(db);
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        final SelectionBuilder builder = buildSelection(uri);
+        getContext().getContentResolver().notifyChange(uri, null);
+        return builder.where(selection, selectionArgs).update(db, values);
+
+    }
+    private SelectionBuilder buildSelection(Uri uri) {
+        final SelectionBuilder builder = new SelectionBuilder();
+        final int match = sUriMatcher.match(uri);
+        return buildSelection(uri, match, builder);
+    }
+
+    private SelectionBuilder buildSelection(Uri uri, int match, SelectionBuilder builder) {
+        final List<String> paths = uri.getPathSegments();
+        switch (match) {
+            case ITEMS: {
+                return builder.table(Tables.ITEMS);
+            }
+            case ITEMS_ID: {
+                final String _id = paths.get(1);
+                return builder.table(Tables.ITEMS).where(ActivityItemsContract.Items._ID + "=?", _id);
+            }
+            default: {
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+            }
+        }
+    }
+    /**
+     * Apply the given set of {@link ContentProviderOperation}, executing inside
+     * a {@link SQLiteDatabase} transaction. All changes will be rolled back if
+     * any single one fails.
+     */
+    public ContentProviderResult[] applyBatch(ArrayList<ContentProviderOperation> operations)
+            throws OperationApplicationException {
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        db.beginTransaction();
+        try {
+            final int numOperations = operations.size();
+            final ContentProviderResult[] results = new ContentProviderResult[numOperations];
+            for (int i = 0; i < numOperations; i++) {
+                results[i] = operations.get(i).apply(this, results, i);
+            }
+            db.setTransactionSuccessful();
+            return results;
+        } finally {
+            db.endTransaction();
+        }
+    }
+}
