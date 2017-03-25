c7ab3605e6f7b2658bbba2422e3f0c2d5ecd8682
OlgaKuklina <okuklina@hotmail.com>
2017-03-24 22:46:22 -0700

    - implemented GitHub user location parser

--- app/src/main/java/com/oklab/githubjourney/asynctasks/UserProfileAsyncTask.java
+++ app/src/main/java/com/oklab/githubjourney/asynctasks/UserProfileAsyncTask.java
@@ -6,11 +6,13 @@ import android.os.AsyncTask;
 import android.util.Log;
 
 import com.oklab.githubjourney.R;
+import com.oklab.githubjourney.data.GitHubUserLocationDataEntry;
 import com.oklab.githubjourney.data.HTTPConnectionResult;
 import com.oklab.githubjourney.data.GitHubUsersDataEntry;
 import com.oklab.githubjourney.data.UserSessionData;
 import com.oklab.githubjourney.services.FollowersParser;
 import com.oklab.githubjourney.services.HTTPConnectionFetcher;
+import com.oklab.githubjourney.services.LocationDataParser;
 import com.oklab.githubjourney.utils.Utils;
 
 import org.json.JSONArray;
@@ -23,7 +25,7 @@ import java.util.List;
  * Created by olgakuklina on 2017-03-21.
  */
 
-public class UserProfileAsyncTask extends AsyncTask<Integer, Void, List<GitHubUsersDataEntry>> {
+public class UserProfileAsyncTask extends AsyncTask<Integer, Void, List<GitHubUserLocationDataEntry>> {
 
     private static final String TAG = UserProfileAsyncTask.class.getSimpleName();
     private final Context context;
@@ -44,9 +46,9 @@ public class UserProfileAsyncTask extends AsyncTask<Integer, Void, List<GitHubUs
     }
 
     @Override
-    protected List<GitHubUsersDataEntry> doInBackground(Integer... args) {
+    protected List<GitHubUserLocationDataEntry> doInBackground(Integer... args) {
         Integer page = args[0];
-        String Uri = context.getString(R.string.url_followers, page);
+        String Uri = context.getString(R.string.url_users, page, currentSessionData.getLogin());
         HTTPConnectionFetcher connectionFetcher = new HTTPConnectionFetcher(Uri, currentSessionData);
         HTTPConnectionResult result = connectionFetcher.establishConnection();
         Log.v(TAG, "responseCode = " + result.getResponceCode());
@@ -54,7 +56,7 @@ public class UserProfileAsyncTask extends AsyncTask<Integer, Void, List<GitHubUs
 
         try {
             JSONArray jsonArray = new JSONArray(result.getResult());
-            return new FollowersParser().parse(jsonArray);
+            return new LocationDataParser().parse(jsonArray);
 
         } catch (JSONException e) {
             Log.e(TAG, "", e);
@@ -64,12 +66,12 @@ public class UserProfileAsyncTask extends AsyncTask<Integer, Void, List<GitHubUs
 
 
     @Override
-    protected void onPostExecute(List<GitHubUsersDataEntry> entryList) {
+    protected void onPostExecute(List<GitHubUserLocationDataEntry> entryList) {
         super.onPostExecute(entryList);
         listener.OnProfilesLoaded(entryList);
     }
 
     public interface OnProfilesLoadedListener {
-        void OnProfilesLoaded(List<GitHubUsersDataEntry> followersDataEntry);
+        void OnProfilesLoaded(List<GitHubUserLocationDataEntry> followersDataEntry);
     }
 }
---
+++ app/src/main/java/com/oklab/githubjourney/services/LocationDataParser.java
@@ -0,0 +1,56 @@
+package com.oklab.githubjourney.services;
+import android.util.Log;
+import com.oklab.githubjourney.data.GitHubUserLocationDataEntry;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by olgakuklina on 2017-03-24.
+ */
+
+public class LocationDataParser {
+    private static final String TAG = LocationDataParser.class.getSimpleName();
+
+    public List<GitHubUserLocationDataEntry> parse(JSONArray jsonArray) throws JSONException {
+
+        List<GitHubUserLocationDataEntry> dataEntriesList = new ArrayList<>(jsonArray.length());
+        for (int i = 0; i < jsonArray.length(); i++) {
+            GitHubUserLocationDataEntry entry = parseItem(jsonArray.getJSONObject(i));
+            dataEntriesList.add(entry);
+        }
+        return dataEntriesList;
+    }
+
+    private GitHubUserLocationDataEntry parseItem(JSONObject object) throws JSONException {
+        if (object == null) {
+            return null;
+        }
+        Log.v(TAG, "login = " + object.getString("login"));
+        Log.v(TAG, "JSONObject = " + object);
+        String login = " ";
+        if (!object.getString("login").isEmpty()) {
+            login = object.getString("login");
+        }
+        String avatarUrl = " ";
+        if (!object.getString("avatar_url").isEmpty()) {
+            avatarUrl = object.getString("avatar_url");
+        }
+        String profileUri = " ";
+        if (!object.getString("url").isEmpty()) {
+            profileUri = object.getString("url");
+        }
+        Log.v(TAG, "location = " + object.getString("location"));
+        Log.v(TAG, "JSONObject = " + object);
+        String location = " ";
+        if (!object.getString("location").isEmpty()) {
+            location = object.getString("location");
+        }
+
+        return new GitHubUserLocationDataEntry(login, avatarUrl, profileUri, location);
+    }
+}
