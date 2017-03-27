a4e3f338d2c00f57606356e8d034446a3d3fec7c
OlgaKuklina <okuklina@hotmail.com>
2017-03-27 04:23:03 -0700

    - added initial implementation of user profile layout

---
+++ app/src/main/java/com/oklab/githubjourney/fragments/UserProfileFragment.java
@@ -0,0 +1,12 @@
+package com.oklab.githubjourney.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by olgakuklina on 2017-03-27.
+ */
+
+public class UserProfileFragment extends Fragment {
+    public UserProfileFragment() {
+    }
+}
