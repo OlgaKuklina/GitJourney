f3b1163f91b667b7079ae29d04825f575ac4e778
OlgaKuklina <okuklina@hotmail.com>
2017-03-28 23:30:16 -0700

    - implemented TimeZone settings activity - implemented  general preferences list

--- app/src/main/java/com/oklab/githubjourney/activities/SettingsActivity.java
+++ app/src/main/java/com/oklab/githubjourney/activities/SettingsActivity.java
@@ -192,8 +192,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
             // to their values. When their values change, their summaries are
             // updated to reflect the new value, per the Android Design
             // guidelines.
-            bindPreferenceSummaryToValue(findPreference("example_list"));
-            bindPreferenceSummaryToValue(findPreference("example_list"));
+            bindPreferenceSummaryToValue(findPreference("timezone_list"));
         }
 
         @Override
