c43975e3b3a20532a6b40a9cadd0a4841a53b9d2
OlgaKuklina <okuklina@hotmail.com>
2017-04-29 23:56:40 -0700

    - initial implementation of repo content parser

--- app/src/main/java/com/oklab/githubjourney/parsers/RepoContentParser.java
+++ app/src/main/java/com/oklab/githubjourney/parsers/RepoContentParser.java
@@ -1,10 +1,14 @@
 package com.oklab.githubjourney.parsers;
 
+import android.util.Log;
+
 import com.oklab.githubjourney.data.GitHubRepoContentType;
+import com.oklab.githubjourney.data.ReposDataEntry;
 import com.oklab.githubjourney.data.RepositoryContentDataEntry;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,9 +21,34 @@ public class RepoContentParser {
     private static final String TAG = RepoContentParser.class.getSimpleName();
 
     public List<RepositoryContentDataEntry> parse(JSONArray jsonArray) throws JSONException {
-        List<RepositoryContentDataEntry> list = new ArrayList<RepositoryContentDataEntry>(1);
-        RepositoryContentDataEntry entry = new RepositoryContentDataEntry("sad", "asd", GitHubRepoContentType.DIR, "asd");
-        list.add(entry);
-        return list;
+        List<RepositoryContentDataEntry> dataEntriesList = new ArrayList<>(jsonArray.length());
+        for (int i = 0; i < jsonArray.length(); i++) {
+            RepositoryContentDataEntry entry = parseItem(jsonArray.getJSONObject(i));
+            dataEntriesList.add(entry);
+        }
+        return dataEntriesList;
+    }
+    private RepositoryContentDataEntry parseItem(JSONObject object) throws JSONException {
+        if (object == null) {
+            return null;
+        }
+        String type = " ";
+        if (!object.getString("type").isEmpty()) {
+            type = object.getString("type");
+        }
+        String uri = " ";
+        if (!object.getString("download_url").isEmpty()) {
+            uri = object.getString("download_url");
+        }
+        String name = " ";
+        if (!object.getString("name").isEmpty()) {
+            name = object.getString("name");
+        }
+
+        String path = " ";
+        if (!object.getString("path").isEmpty()) {
+            path = object.getString("path");
+        }
+        return new RepositoryContentDataEntry(name, uri, type, path);
     }
 }
