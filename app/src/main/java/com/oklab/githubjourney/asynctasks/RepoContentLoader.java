4e37610dafd839e8fe1b42ea0df8f8017ed4d37b
OlgaKuklina <okuklina@hotmail.com>
2017-06-24 22:30:55 -0700

    - fixed issue in retriving repository content when path contents a space

--- app/src/main/java/com/oklab/githubjourney/asynctasks/RepoContentLoader.java
+++ app/src/main/java/com/oklab/githubjourney/asynctasks/RepoContentLoader.java
@@ -1,7 +1,9 @@
 package com.oklab.githubjourney.asynctasks;
 
 import android.content.Context;
+import android.net.Uri;
 import android.support.v4.content.AsyncTaskLoader;
+import android.text.TextUtils;
 import android.util.Log;
 
 import com.oklab.githubjourney.R;
@@ -15,6 +17,7 @@ import org.json.JSONException;
 
 import java.util.List;
 
+
 /**
  * Created by olgakuklina on 2017-04-17.
  */
@@ -27,7 +30,12 @@ public class RepoContentLoader extends AsyncTaskLoader<List<RepositoryContentDat
 
     public RepoContentLoader(Context context, String path, String repoName, String userName) {
         super(context);
-        this.path = path;
+
+        String[] pathList = path.split("/");
+        for (int i = 0; i < pathList.length; i++) {
+            pathList[i] = Uri.encode(pathList[i]);
+        }
+        this.path = TextUtils.join("/", pathList);
         this.repoName = repoName;
         this.userName = userName;
     }
